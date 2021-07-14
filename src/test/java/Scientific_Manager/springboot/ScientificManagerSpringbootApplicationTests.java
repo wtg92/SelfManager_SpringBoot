d035ade7eae1c1c943f789c1d8a8ba7a28884e69
wtg92 <wtg92@126.com>
2021-07-14 10:25:27 +0800

    init

---
+++ src/test/java/Scientific_Manager/springboot/ScientificManagerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package Scientific_Manager.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificManagerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
