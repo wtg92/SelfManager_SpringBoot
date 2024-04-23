741b93145aa2d7e85c33102f90a46e0a0a78cc5b
sunny-guopengfei <guop_ssn@anasystems.co.jp>
2024-04-23 14:02:03 +0900

    The most dangerous moment has come

--- src/main/java/manager/util/EmailUtil.java
+++ src/main/java/manager/util/EmailUtil.java
@@ -6,16 +6,12 @@ import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 import com.sun.mail.smtp.SMTPMessage;
 
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import manager.exception.LogicException;
 import manager.system.SMError;
 
